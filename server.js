b5d9d4670ffca2cdaedd9ce0e1b7da3f2db50f7e
Gtax <gtax@Gtaxs-MacBook-Pro.local>
2018-02-11 00:31:25 +0200

    Social Network Alpha

---
+++ server.js
@@ -0,0 +1,38 @@
+const Hapi = require("hapi");
+const server = new Hapi.Server();
+
+server.connection({port:3000});
+
+server.start(console.log("test"));
+server.route({
+	method: "GET",
+	path: "/",
+	handler: function(request,reply){
+		reply.view("landing");
+	}
+})
+
+server.register(require("vision"),function(err){
+	server.views({
+		engines:{
+			ejs:require("ejs")
+		},
+		relativeTo: __dirname,
+		path:"views"
+	})
+});
+
+server.register(require("inert"),function(err){
+
+});
+
+server.route({
+	method:"GET",
+	path:"/{param*}",
+	handler:{
+		directory:{
+			path:"public"
+		}
+	}
+})
+
